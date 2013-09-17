5d8e14370e8f50b590f1b7a803bbd86371240946
Ben Horne <benhorne44@gmail.com>
2013-09-16 21:17:42 -0600

    Added methods load, help, help command, and queue count.

--- event_reporter.rb
+++ event_reporter.rb
@@ -19,65 +19,97 @@ class Queue
 
   def run
      puts "Welcome to EventReporter!"
-    while @input != 'quit'
+    while @command != 'quit'
       printf "enter command:"
       input = gets.chomp
       execute_command(input)
     end
   end
 
-  def execute_command(input)
-    case input
-      when 'quit' then puts "Goodbye!"
-        exit
-      when 'load' then puts "Enter Filename:"
-        input = gets.chomp
-        load(input)
-      return self
-    end
-  end
-
-  def load(filename = "event_attendees.csv")
-    
-    @contents = []
-    
-    @contents = CSV.open "#{filename}", headers: true, header_converters: :symbol  
-    
-    @contents.each do |row|
-      name = row[:first_name]
+  def help(input)
+    @help_list = { "quit" => "exits the program", 
+      "help <command>" => "Outputs a description of a given command.",
+      "help list" => "List the available individual commands.",
+      "queue count" => "Output the number of records in the current queue.",
+      "queue clear" => "Empties the queue.",
+      "queue print" => "Print out queue data table.",
+      "queue print by <attribute>" => "Print the data table sorted by specified attribute.",
+      "queue save to <filename.csv>" => "Export the current queue to the specified filename as a CSV.",
+      "find" => "Load the queue with all records matching the criteria for the given attribute. \n\t'find <attribute> <criteria>'"
+    }
+    if input == ""
+      puts "Please enter a command after 'help', or type 'help list' for more options."
+    elsif input == 'list'
+      puts "#{@help_list.keys}"
+    else
+      puts "#{@help_list[input]}"
     end
 
-  end
 
-  def help
-    help_list
-  end
-
-  def help_list
-    {"quit" => "exits the program", "help" => "outputs a description of given command"}
     # hash
     # output a listing of available commands
     # output a description of command
   end
 
-  def queue_count
-    @queue.count
+  def execute_command(input)
+    parts = input.split(' ')
+    @command = parts[0]
+    @message = parts [1..-1].join(" ")
+
+    # if @message == "" && @command != 'help' && @command != 'quit'
+    #   puts "Please enter a message with your command."
+    case @command
+      when 'quit' then puts "Goodbye!"
+        exit
+      when 'load' then load(@message)
+      when 'help' then help(@message)
+      when 'queue' then queue(@message)
+      return self
+    end
   end
 
-  def queue_clear
-    @queue = []
-  end
-
-  def queue_print
-    # Print out a tab-delimited data table with a header row following this format:
-  end
-
-  def queue_print(attribute)
+  def load(filename)
+    if filename == ""
+      filename = "event_attendees.csv"
+    end
+    @contents = CSV.open "#{filename}", headers: true, header_converters: :symbol  
     
+    @contents.each do |row|
+      first_name = row[:first_name]
+
+      puts first_name
+    end
+
   end
 
-  def queue_save_file
-    output = File.open(filename, 'w')
+  
+  def queue(input)
+    case input 
+      when "" then puts "Please specify your command after 'queue'"
+      when 'count' then puts @queue.count
+      when 'print' then 
+        @contents.each do |row|
+          first_name = row[:first_name]
+
+          puts name
+        end
+      end
+
+    # def queue_clear
+    #   @queue = []
+    # end
+
+    # def queue_print
+    #   # Print out a tab-delimited data table with a header row following this format:
+    # end
+
+    # def queue_print(attribute)
+      
+    # end
+
+    # def queue_save_file
+    #   output = File.open(filename, 'w')
+    # end
   end
 
 end
